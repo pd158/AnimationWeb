be5426428cb6dcf41d7a9bef1ee4de0f56982bb8
pd158 <pavi_96@live.co.uk>
2019-08-12 22:07:31 +0100

    progress of setting up signup page with database

--- Video/database.php
+++ Video/database.php
@@ -5,16 +5,28 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $db = "planet";
+
 
     // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $db);
+    $conn = mysqli_connect($servername, $username, $password);
+
+    //select SQL Database
+    mysqli_select_db($con, 'planet');
 
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
     echo "Connected successfully";
+
+    $name = $_POST['user'];
+    $pass = $_POST['password'];
+
+    $s = " select * from customer where name -'$name'";
+
+    $result = mysqli_query($con, $s);
+
+    $num = mysqli_num_rows
 
 
     ?>
